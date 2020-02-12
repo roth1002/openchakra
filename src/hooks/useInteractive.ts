df52bbfa9b504dc3557f653648e70e483e81456a
Pierre <pierre.crbs@gmail.com>
2020-02-12 16:59:32 +0100

    update focus

--- src/hooks/useInteractive.ts
+++ src/hooks/useInteractive.ts
@@ -31,6 +31,7 @@ export const useInteractive = (
       event.preventDefault()
       event.stopPropagation()
       dispatch.components.select(component.id)
+      dispatch.app.toggleInputText()
     },
     onDoubleClick: (event: MouseEvent) => {
       event.preventDefault()
